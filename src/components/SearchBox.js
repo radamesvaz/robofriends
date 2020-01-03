884dd4bf230ef4ab98f473ca1e249ef09cfbf52b
Radames Vaz <rgvtovar@hotmail.com>
2020-01-03 19:51:25 -0400

    probando mas cambios

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,11 +3,11 @@ import React from 'react';
 const SearchBox = ({searchfield, searchChange}) => {
     return(
         <div className = 'pa2'>
-        <input className = 'pa3 ba b--green bg-lightest-blue br4' 
+        <h3><input className = 'br4 pa3 ba b--green bg-lightest-blue' 
         type = 'search' 
         placeholder='Search Robots' 
         onChange={searchChange}    
-        />
+        /></h3>
         </div>
     );
 }
